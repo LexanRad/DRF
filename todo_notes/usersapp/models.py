8ca8c1be0852acd5ace841915739be71fe142a76
Lexan <sheklford2015@yandex.ru>
2022-02-22 15:41:56 +0300

    create model user

--- todo_notes/usersapp/models.py
+++ todo_notes/usersapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=64, unique=True)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
