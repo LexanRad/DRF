5ecdae0ee8f54a7860ccf46e586e65cd7d4c5146
Lexan <sheklford2015@yandex.ru>
2022-02-28 16:12:21 +0300

    Create menu

---
+++ todo_notes/frontend/src/components/Menu.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MenuItem = ({menu}) => {
+    return (
+        <menu>
+            <a href="#">
+                Что-то в меню
+            </a>
+
+            <a href="#">
+                Что-то № 2
+            </a>
+        </menu>
+    )
+}
+
+
+export default MenuItem;
