583cc9abe4d0b59b34526c1fbab65d001409a944
Lexan <sheklford2015@yandex.ru>
2022-03-09 12:55:10 +0300

    Refactor views

--- todo_notes/usersapp/views.py
+++ todo_notes/usersapp/views.py
@@ -1,8 +1,17 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework import mixins
+
 from .models import User
 from .serializers import UserModelSerializer
 
 
-class UserModelViewSet(ModelViewSet):
+# class UserModelViewSet(ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserModelSerializer
+
+class UserGenericViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
+                               mixins.UpdateModelMixin, GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
