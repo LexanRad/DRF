65819d09682cf5e80e384ec0beac2001dcb82306
Lexan <sheklford2015@yandex.ru>
2022-04-08 14:26:22 +0300

    add documentation with ddrf-yasg

--- todo_notes/todo_notes/urls.py
+++ todo_notes/todo_notes/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView,)
@@ -26,6 +29,18 @@ router.register('users', UserGenericViewSet)
 router.register('projects', ProjectModelViewSet)
 router.register('todo', ToDoModelViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ToDo",
+      default_version='0.1',
+      description="Documentation to out project",
+      contact=openapi.Contact(email="admin@admin.local"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,5 +48,12 @@ urlpatterns = [
     path('api-auth-token/', views.obtain_auth_token),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include(router.urls))
+    re_path(r'^api/(?P<version>\d\.\d)/users/$', UserGenericViewSet.as_view({'get': 'list'})),
+    path('api/users/0.1', include('users.urls', namespace='0.1')),
+    path('api/users/0.2', include('users.urls', namespace='0.2')),
+    path('api/', include(router.urls)),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
